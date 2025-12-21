f683d0dada5bd4e7312ecba76d976a8e46e95586
katieprogrammes <katiehayman98@gmail.com>
2025-12-21 15:39:58 +0000

    Add project files.

---
+++ LearnGL/main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+//Called on window resize
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
